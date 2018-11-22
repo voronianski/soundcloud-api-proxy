10dba1937b92dbc91195738837dc433d9e70e99e
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-11-22 13:32:50 +0100

    improvements

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# SoundCloud API Proxy
+# <img src="http://www.officialpsds.com/images/thumbs/Soundcloud-Logo-psd47614.png" width="75" align="left">&nbsp;SoundCloud API Proxy
 
 > Just a simple proxy server for not letting to abuse my SoundCloud client secrets.
 
 <img src="https://media.giphy.com/media/rxZ4RFo1Vj9ao/giphy.gif" width="500" />
 
 ---
-
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const logger = require('morgan');
-const request = require('request');
 const cors = require('cors');
+const logger = require('morgan');
+const express = require('express');
+const request = require('request');
 
 const PORT = process.env.PORT || 1984;
 const CLIENT_ID = process.env.CLIENT_ID;
 const API_URL = process.env.API_URL || 'https://api.soundcloud.com';
-const WHITELIST = [
-  'voronianski.com',
+const WHITELIST = (process.env.WHITELIST || []).split(',') || [
   'localhost',
-  '127.0.0.1',
-  '0.0.0.0'
+  '127.0.0.1'
 ];
 
 const app = express();
@@ -21,31 +19,35 @@ app.get('/resolve', handleResolve);
 app.get('/stream/:trackId', handleStream);
 app.use(handleMisc);
 
-function corsDelegate (req, done) {
+function corsDelegate(req, done) {
   const corsOptions = { origin: false };
 
   if (WHITELIST.includes(req.header('Origin')) !== -1) {
     corsOptions.origin = true;
-  } else{
+  } else {
     corsOptions.origin = false;
   }
 
   done(null, corsOptions);
 }
 
-function handleResolve (req, res) {
-  const resolver = `${API_URL}/resolve.json?url=${req.query.url}&client_id=${CLIENT_ID}`;
+function handleResolve(req, res) {
+  const resolver = `${API_URL}/resolve.json?url=${
+    req.query.url
+  }&client_id=${CLIENT_ID}`;
 
   return request(resolver).pipe(res);
 }
 
-function handleStream (req, res) {
-  const stream = `${API_URL}/tracks/${req.params.trackId}/stream?client_id=${CLIENT_ID}`;
+function handleStream(req, res) {
+  const stream = `${API_URL}/tracks/${
+    req.params.trackId
+  }/stream?client_id=${CLIENT_ID}`;
 
   return request(stream).pipe(res);
 }
 
-function handleMisc (req, res) {
+function handleMisc(req, res) {
   const giffy = 'https://media.giphy.com/media/rxZ4RFo1Vj9ao/giphy.gif';
 
   return request(giffy).pipe(res);
